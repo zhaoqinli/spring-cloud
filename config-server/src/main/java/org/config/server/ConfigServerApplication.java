d93873f9e58b54494636a606ad2483b4ecc99b85
shishuang <shishuang@j1.com>
2017-12-28 14:34:07 +0800

    1.0

---
+++ config-server/src/main/java/org/config/server/ConfigServerApplication.java
@@ -0,0 +1,25 @@
+package org.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
+/**
+ * http请求地址和资源文件映射如下:
+ * 
+ * /{application}/{profile}[/{label}] 
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml 
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ *
+ * 本项目实例：http://localhost:8888/config-client/dev
+ */
